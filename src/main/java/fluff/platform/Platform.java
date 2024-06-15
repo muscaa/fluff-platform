ae90cb8420bdbc440263bf3d402f66957ae0d0e2
Musca Mihail <muscamihaill@gmail.com>
2024-06-15 16:57:06 +0300

    Added default dir

--- src/main/java/fluff/platform/Platform.java
+++ src/main/java/fluff/platform/Platform.java
@@ -43,6 +43,17 @@ public class Platform {
     }
     
     /**
+     * Extracts the native library to the default directory.
+     *
+     * @param lib the native library to extract
+     * @param overwrite whether to overwrite the file if it already exists
+     * @return true if the library was successfully extracted, false otherwise
+     */
+    public static boolean extract(INativeLibrary lib, boolean overwrite) {
+        return extract(PlatformConfig.DEFAULT_DIR, lib, overwrite);
+    }
+    
+    /**
      * Extracts the native library to the specified directory without overwriting existing files.
      *
      * @param dir the directory to extract the library to
@@ -51,6 +62,16 @@ public class Platform {
      */
     public static boolean extract(File dir, INativeLibrary lib) {
         return extract(dir, lib, false);
+    }
+    
+    /**
+     * Extracts the native library to the default directory without overwriting existing files.
+     *
+     * @param lib the native library to extract
+     * @return true if the library was successfully extracted, false otherwise
+     */
+    public static boolean extract(INativeLibrary lib) {
+        return extract(PlatformConfig.DEFAULT_DIR, lib);
     }
     
     /**
@@ -75,6 +96,16 @@ public class Platform {
     }
     
     /**
+     * Loads the native library from the default directory.
+     *
+     * @param lib the native library to load
+     * @return true if the library was successfully loaded, false otherwise
+     */
+    public static boolean load(INativeLibrary lib) {
+        return load(PlatformConfig.DEFAULT_DIR, lib);
+    }
+    
+    /**
      * Extracts and loads the native library from the specified directory.
      *
      * @param dir the directory to extract and load the library from
@@ -83,6 +114,16 @@ public class Platform {
      */
     public static boolean extractAndLoad(File dir, INativeLibrary lib) {
         return extract(dir, lib) ? load(dir, lib) : false;
+    }
+    
+    /**
+     * Extracts and loads the native library from the default directory.
+     *
+     * @param lib the native library to extract and load
+     * @return true if the library was successfully extracted and loaded, false otherwise
+     */
+    public static boolean extractAndLoad(INativeLibrary lib) {
+        return extractAndLoad(PlatformConfig.DEFAULT_DIR, lib);
     }
     
     /**
---
+++ src/main/java/fluff/platform/PlatformConfig.java
@@ -0,0 +1,8 @@
+package fluff.platform;
+
+import java.io.File;
+
+public class PlatformConfig {
+	
+	public static File DEFAULT_DIR = new File("natives");
+}
